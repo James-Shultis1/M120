fb879ea0b2a1887a2499483f6c6b2b5136cabccd
James-Shultis1 <47882320+James-Shultis1@users.noreply.github.com>
2019-05-07 10:33:57 +0200

    Update Books.php

--- Books.php
+++ Books.php
@@ -9,7 +9,69 @@
     <?php include "Header.php";
     include "Menu.php"; ?>
 
-    BOOOOOOKSSSS!!!!
+    <div class="bookCont" id="b1">
+
+    </div>
+    <div class="bookCont" id="b2">
+
+    </div>
+    <div class="bookCont" id="b3">
+
+    </div>
+    <div class="bookCont" id="b4">
+
+    </div>
+    <div class="bookCont" id="b5">
+
+    </div>
+    <div class="bookCont" id="b6">
+
+    </div>
+    <div class="bookCont" id="b7">
+
+    </div>
+    <div class="bookCont" id="b8">
+
+    </div>
+    <div class="bookCont" id="b9">
+
+    </div>
+    <div class="bookCont" id="b10">
+
+    </div>
+    <div class="bookCont" id="b11">
+
+    </div>
+    <div class="bookCont" id="b12">
+
+    </div>
+    <div class="bookCont" id="b13">
+
+    </div>
+    <div class="bookCont" id="b14">
+
+    </div>
+    <div class="bookCont" id="b15">
+
+    </div>
+    <div class="bookCont" id="b16">
+
+    </div>
+    <div class="bookCont" id="b17">
+
+    </div>
+    <div class="bookCont" id="b18">
+
+    </div>
+    <div class="bookCont" id="b19">
+
+    </div>
+    <div class="bookCont" id="b20">
+
+    </div>
+    <div class="bookCont" id="b21">
+
+    </div>
 
     <br> <?php include "Footer.php"; ?>
   </body>
