75b871e128cf7dc338fd3aae36d58e997dc247c4
MarceloGuia <marcelo.guia.mg@gmail.com>
2019-05-07 12:36:42 +0200

    Added the div's to the pagination logic.

--- Books.php
+++ Books.php
@@ -13,70 +13,6 @@ session_start();
     <?php include "Header.php";
     // include "Menu.php"; ?>
     <div class="books">
-      <div class="bookCont" id="b1">
-
-      </div>
-      <div class="bookCont" id="b2">
-
-      </div>
-      <div class="bookCont" id="b3">
-
-      </div>
-      <div class="bookCont" id="b4">
-
-      </div>
-      <div class="bookCont" id="b5">
-
-      </div>
-      <div class="bookCont" id="b6">
-
-      </div>
-      <div class="bookCont" id="b7">
-
-      </div>
-      <div class="bookCont" id="b8">
-
-      </div>
-      <div class="bookCont" id="b9">
-
-      </div>
-      <div class="bookCont" id="b10">
-
-      </div>
-      <div class="bookCont" id="b11">
-
-      </div>
-      <div class="bookCont" id="b12">
-
-      </div>
-      <div class="bookCont" id="b13">
-
-      </div>
-      <div class="bookCont" id="b14">
-
-      </div>
-      <div class="bookCont" id="b15">
-
-      </div>
-      <div class="bookCont" id="b16">
-
-      </div>
-      <div class="bookCont" id="b17">
-
-      </div>
-      <div class="bookCont" id="b18">
-
-      </div>
-      <div class="bookCont" id="b19">
-
-      </div>
-      <div class="bookCont" id="b20">
-
-      </div>
-      <div class="bookCont" id="b21">
-
-      </div>
-    </div>
 
     <?php
     // This part was taken from Marcelo's previous project. It was adapted to fit the requirements
@@ -101,7 +37,7 @@ session_start();
             // minus 1 multiplied the number of articles per page. This is done like this because the number of the offset will be 1 below the number the SQL will
             // start pulling results from and if we're on page one for example, we only want Articles 1-5, so 1 - 1 * 5 = 0, which means the offset will be 0 and
             // the SQL will pull the first 5 results, without skipping any. On page 2 that would be 2 - 1 * 5 = 5, so we would skip the first 5.
-            $ArticlesPerPage = 20;
+            $ArticlesPerPage = 21;
             $Offset = ($PageNr-1) * $ArticlesPerPage;
 
             // So here we want to know the amount of articles in order to save it into a variable totalrow.
@@ -115,14 +51,20 @@ session_start();
             // Here we finally put the use of offset into practice. The mysqli query is used to fetch the results.
             $sql = "SELECT * FROM buecher LIMIT $Offset, $ArticlesPerPage";
             $result = mysqli_query($conn,$sql);
+            $idNum = 0;
             while($row = mysqli_fetch_array($result))
             {// Nice and tidy like they are in the Home Page, the same way.?>
 
-              Note to self: Add in Layout of the books!
+              <div class="bookCont" id="b<<?php echo $idNum; ?>">
+
+              </div>
 
                 <?php
-            }
+                $idNum++;
+            } ?>
+            </div>
 
+            <?php
             // Here we use the lists and links to create the pagination code. The links function similarly to the chapter links, but instead we're using our
             // own variable, which defines which page we're on. I also added in a little extra from myself to display the actual pages around the one the user
             // is currently on. These are also clickable and function the same way.
