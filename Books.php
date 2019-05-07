3271cf0e0c290758c2ae636855d3f3e79080c3e4
MarceloGuia <marcelo.guia.mg@gmail.com>
2019-05-07 10:16:30 +0200

    Create Books.php

---
+++ Books.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Antiquities</title>
+    <link rel="stylesheet" href="CSS/CSS.css">
+  </head>
+  <body>
+    <?php include "Header.php";
+    include "Menu.php"; ?>
+
+    BOOOOOOKSSSS!!!!
+
+    <br> <?php include "Footer.php"; ?>
+  </body>
+</html>
