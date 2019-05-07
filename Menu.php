62f6e47682b7fb9af323942d7865a77290a2a176
MarceloGuia <marcelo.guia.mg@gmail.com>
2019-05-07 10:16:47 +0200

    Create Menu.php

---
+++ Menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+
+    <div id="Menu">
+      <div class="MenuContent">
+        <a href="index.php">HOT BOOKS!</a><br>
+      </div>
+      <div class="MenuContent">
+        <a href="Books.php">All Books</a><br>
+      </div>
+      <div class="MenuContent">
+        <a href="Admin.php">Administration</a>
+      </div>
+    </div>
+
+  </body>
+</html>
