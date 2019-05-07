f61371ebd216383ede31ea828859f22fca857a2a
MarceloGuia <marcelo.guia.mg@gmail.com>
2019-05-07 10:16:45 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hottest Books!</title>
+    <link rel="stylesheet" href="CSS/CSS.css">
+  </head>
+  <body>
+    <?php include "Header.php";
+    include "Menu.php"; ?>
+
+    Content
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br>
+    y
+
+    <br> <?php include "Footer.php"; ?>
+  </body>
+</html>
